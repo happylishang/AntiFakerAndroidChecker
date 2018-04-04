e8158aba18ab6e7055938bf52283507df847e955
happylishang <happylishang@163.com>
2018-04-04 18:53:39 +0800

    好奇葩，加个log就稳定的多

--- app/src/main/jni/emulator/com.snail.device_jni_EmulatorDetectUtil.c
+++ app/src/main/jni/emulator/com.snail.device_jni_EmulatorDetectUtil.c
@@ -82,7 +82,7 @@ int detect() {
     //如果不是 (size_t) getpagesize() 是sizeof（code），就必须加上LOGI(" mmap sucess exec  %x", exec); ，才能降低崩溃概率，这尼玛操蛋
     asmcheck = (int *) exec;
     LOGI(" start asmcheck " );
-   // a= asmcheck();
+    a= asmcheck();
     munmap(exec, getpagesize());
     return a;
 }
