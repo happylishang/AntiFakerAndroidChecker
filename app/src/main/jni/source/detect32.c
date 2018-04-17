15f3f0b498703a2cb8ed3a14e7927525e7e3032b
happylishang <happylishang@163.com>
2018-04-17 20:53:52 +0800

    高级

--- app/src/main/jni/source/detect32.c
+++ app/src/main/jni/source/detect32.c
@@ -40,8 +40,6 @@ int detect (){
  //     -d -j section_name：反汇编指定的section；
 
 
-//
-//
 //00000000 <detect>:
 //        0:	e52db004 	push	{fp}		; (str fp, [sp, #-4]!)
 //        4:	e28db000 	add	fp, sp, #0
@@ -50,39 +48,30 @@ int detect (){
 //        10:	e50b3008 	str	r3, [fp, #-8]
 //        14:	e92d41f0 	push	{r4, r5, r6, r7, r8, lr}
 //        18:	e3a07000 	mov	r7, #0
-//        1c:	e1a0800f 	mov	r8, pc
-//        20:	e3a04000 	mov	r4, #0
-//        24:	e2877001 	add	r7, r7, #1
+//        1c:	e3a04000 	mov	r4, #0
+//
+//        00000020 <smc11>:
+//        20:	e2877001 	add	r7, r7, #1
+//        24:	e24f800c 	sub	r8, pc, #12
 //        28:	e5985000 	ldr	r5, [r8]
 //
-//        0000002c <code>:
+//        0000002c <code11>:
 //        2c:	e2844001 	add	r4, r4, #1
-//        30:	e1a0800f 	mov	r8, pc
-//        34:	e248800c 	sub	r8, r8, #12
-//        38:	e5885000 	str	r5, [r8]
-//        3c:	e354000a 	cmp	r4, #10
-//        40:	aa000002 	bge	50 <out>
-//        44:	e357000a 	cmp	r7, #10
-//        48:	aa000000 	bge	50 <out>
-//        4c:	eafffff6 	b	2c <code>
+//        30:	e24f800c 	sub	r8, pc, #12
+//        34:	e5885000 	str	r5, [r8]
+//        38:	e354000a 	cmp	r4, #10
+//        3c:	aa000002 	bge	4c <out11>
+//  40:	e357000a 	cmp	r7, #10
+//          44:	aa000000 	bge	4c <out11>
+//  48:	eafffff7 	b	2c <code11>
 //
-//00000050 <out>:
-//        50:	e1a00004 	mov	r0, r4
-//        54:	e8bd81f0 	pop	{r4, r5, r6, r7, r8, pc}
-//        58:	e1a03000 	mov	r3, r0
-//        5c:	e50b3008 	str	r3, [fp, #-8]
-//        60:	e51b3008 	ldr	r3, [fp, #-8]
-//        64:	e1a00003 	mov	r0, r3
-//        68:	e24bd000 	sub	sp, fp, #0
-//        6c:	e49db004 	pop	{fp}		; (ldr fp, [sp], #4)
-//        70:	e12fff1e 	bx	lr
-//
-//        00000074 <main>:
-//        74:	e92d4800 	push	{fp, lr}
-//        78:	e28db004 	add	fp, sp, #4
-//        7c:	ebfffffe 	bl	0 <detect>
-//  80:	e1a03000 	mov	r3, r0
-//          84:	e1a00000 	nop			; (mov r0, r0)
-//          88:	e1a00003 	mov	r0, r3
-//          8c:	e8bd8800 	pop	{fp, pc}
-
+//0000004c <out11>:
+//        4c:	e1a00004 	mov	r0, r4
+//        50:	e8bd81f0 	pop	{r4, r5, r6, r7, r8, pc}
+//        54:	e1a03000 	mov	r3, r0
+//        58:	e50b3008 	str	r3, [fp, #-8]
+//        5c:	e51b3008 	ldr	r3, [fp, #-8]
+//        60:	e1a00003 	mov	r0, r3
+//        64:	e24bd000 	sub	sp, fp, #0
+//        68:	e49db004 	pop	{fp}		; (ldr fp, [sp], #4)
+//        6c:	e12fff1e 	bx	lr
