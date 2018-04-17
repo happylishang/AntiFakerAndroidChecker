2bb35d5869f8b2b91930ee4e1fd91b391c9aaa95
happylishang <happylishang@163.com>
2018-04-17 17:26:53 +0800

    adf

--- app/src/main/jni/emulator/com.snail.device_jni_EmulatorDetectUtil.c
+++ app/src/main/jni/emulator/com.snail.device_jni_EmulatorDetectUtil.c
@@ -43,37 +43,33 @@ int b = -1;
 int (*asmcheck)(void);
 
 
+
+
+
+
 int detect1 (){
     int a=0;    //声明出口参数
     __asm __volatile ( //这段属于self-modifing-code 自修改代码
 
             "sub	    sp, sp, #0x30 \n"
             "stp    x29, x30, [sp, #0x20]\n"
-
             "smc%=:\n"
-
             "mov x0,#0 \n"
             "ADR x1,smc%=\n"
             "mov x2,#0 \n"
             "add x0,x0,#1 \n"
             "ldr x3,[x1]\n"
-
             "code%=:\n"
-
             "add x2,x2,#1\n"
             "ADR x1,code%=\n"
             "sub x1,x1,#12\n"
-       //     "str x3,[x1]\n"
-
+            "str x3,[x1]\n"
             "cmp x2,#10\n"
             "bge out%=\n"
-
             "cmp x0,#10\n"
             "bge out%=\n"
             "b code%=\n"
-
             "out%=:\n"
-
             "mov x0,x2\n"
             "ldp    x29, x30, [sp, #0x20]  \n"
             "add    sp, sp, #0x30   \n"
@@ -87,8 +83,6 @@ int detect1 (){
 
 int detect() {
 
-
-char code[] =
 
     a =detect1() ;
     return a;
@@ -110,45 +104,5 @@ JNIEXPORT jboolean JNICALL Java_com_snail_device_jni_EmulatorDetectUtil_detect
 
 
 
-//detect_func.o:     file format elf32-littlearm
-//
-//
-//Disassembly of section .text:
-//
-//00000000 <detect>:
-//   0:	e52db004 	push	{fp}		; (str fp, [sp, #-4]!)
-//   4:	e28db000 	add	fp, sp, #0
-//   8:	e24dd00c 	sub	sp, sp, #12
 
-//   c:	e3a03000 	mov	r3, #0
-//  10:	e50b3008 	str	r3, [fp, #-8]
 
-//  14:	e92d41f0 	push	{r4, r5, r6, r7, r8, lr}
-//  18:	e3a07000 	mov	r7, #0
-//  1c:	e1a0800f 	mov	r8, pc
-//  20:	e3a04000 	mov	r4, #0
-//  24:	e2877001 	add	r7, r7, #1
-//  28:	e5985000 	ldr	r5, [r8]
-//
-//0000002c <code>:
-//  2c:	e2844001 	add	r4, r4, #1
-//  30:	e1a0800f 	mov	r8, pc
-//  34:	e248800c 	sub	r8, r8, #12
-//  38:	e5885000 	str	r5, [r8]
-//  3c:	e354000a 	cmp	r4, #10
-//  40:	aa000002 	bge	50 <out>
-//  44:	e357000a 	cmp	r7, #10
-//  48:	aa000000 	bge	50 <out>
-//  4c:	eafffff6 	b	2c <code>
-//
-//00000050 <out>:
-//  50:	e1a00004 	mov	r0, r4
-//  54:	e8bd81f0 	pop	{r4, r5, r6, r7, r8, pc}
-
-//  58:	e1a03000 	mov	r3, r0
-//  5c:	e50b3008 	str	r3, [fp, #-8]
-//  60:	e3a03000 	mov	r3, #0
-//  64:	e1a00003 	mov	r0, r3
-//  68:	e28bd000 	add	sp, fp, #0
-//  6c:	e8bd0800 	pop	{fp}
-//  70:	e12fff1e 	bx	lr
--- app/src/main/jni/source/detect64.c
+++ app/src/main/jni/source/detect64.c
@@ -44,43 +44,25 @@ return detect ();
 //      -D：反汇编全部；
  //     -d -j section_name：反汇编指定的section；
 
-
-//
-//
-
-//0000000000000000 <detect>:
-//        0:	d10043ff 	sub	sp, sp, #0x10
-//        4:	b9000fff 	str	wzr, [sp,#12]
-
-//        8:	d100c3ff 	sub	sp, sp, #0x30
-//        c:	a9027bfd 	stp	x29, x30, [sp,#32]
-//
-//        0000000000000010 <smc>:
-//           10:	d2800000 	mov	x0, #0x0                   	// #0
-//          14:	10ffffe1 	adr	x1, 10 <smc>
-//          18:	d2800002 	mov	x2, #0x0                   	// #0
-//          1c:	91000400 	add	x0, x0, #0x1
-//          20:	f9400023 	ldr	x3, [x1]
-//
-//          0000000000000024 <code>:
-//          24:	91000442 	add	x2, x2, #0x1
-//          28:	10ffffe1 	adr	x1, 24 <code>
-//          2c:	d1003021 	sub	x1, x1, #0xc
-//          30:	f9000023 	str	x3, [x1]
-//          34:	f100285f 	cmp	x2, #0xa
-//          38:	5400008a 	b.ge	48 <out>
-//          3c:	f100281f 	cmp	x0, #0xa
-//          40:	5400004a 	b.ge	48 <out>
-//          44:	17fffff8 	b	24 <code>
-//
-//0000000000000048 <out>:
-//        48:	aa0203e0 	mov	x0, x2
-//        4c:	a9427bfd 	ldp	x29, x30, [sp,#32]
-//        50:	9100c3ff 	add	sp, sp, #0x30
-
-
-//        54:	aa0003e0 	mov	x0, x0
-//        58:	b9000fe0 	str	w0, [sp,#12]
-//        5c:	b9400fe0 	ldr	w0, [sp,#12]
-//        60:	910043ff 	add	sp, sp, #0x10
-//        64:	d65f03c0 	ret
+//000000000000099c <detect1@@Base>:
+//        99c:	d100c3ff 	sub	sp, sp, #0x30
+//        9a0:	a9027bfd 	stp	x29, x30, [sp,#32]
+//        9a4:	d2800000 	mov	x0, #0x0                   	// #0
+//        9a8:	10ffffe1 	adr	x1, 9a4 <detect1@@Base+0x8>
+//        9ac:	d2800002 	mov	x2, #0x0                   	// #0
+//        9b0:	91000400 	add	x0, x0, #0x1
+//        9b4:	f9400023 	ldr	x3, [x1]
+//        9b8:	91000442 	add	x2, x2, #0x1
+//        9bc:	10ffffe1 	adr	x1, 9b8 <detect1@@Base+0x1c>
+//        9c0:	d1003021 	sub	x1, x1, #0xc
+//        9c4:	f9000023 	str	x3, [x1]
+//        9c8:	f100285f 	cmp	x2, #0xa
+//        9cc:	5400008a 	b.ge	9dc <detect1@@Base+0x40>
+//        9d0:	f100281f 	cmp	x0, #0xa
+//        9d4:	5400004a 	b.ge	9dc <detect1@@Base+0x40>
+//        9d8:	17fffff8 	b	9b8 <detect1@@Base+0x1c>
+//        9dc:	aa0203e0 	mov	x0, x2
+//        9e0:	a9427bfd 	ldp	x29, x30, [sp,#32]
+//        9e4:	9100c3ff 	add	sp, sp, #0x30
+//        9e8:	aa0003e0 	mov	x0, x0
+//        9ec:	d65f03c0 	ret
