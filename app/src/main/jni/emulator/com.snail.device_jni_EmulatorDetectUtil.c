9b334806d33fbd55e9b9eadfbaed88b97b734284
happylishang <happylishang@163.com>
2018-04-18 10:02:16 +0800

    去

--- app/src/main/jni/emulator/com.snail.device_jni_EmulatorDetectUtil.c
+++ app/src/main/jni/emulator/com.snail.device_jni_EmulatorDetectUtil.c
@@ -45,31 +45,26 @@ int (*asmcheck)(void);
 int detectAsm (){
     int a=0;    //声明出口参数
     __asm __volatile ( //这段属于self-modifing-code 自修改代码
-
-            "sub	    sp, sp, #0x30 \n"
-            "stp    x29, x30, [sp, #0x20]\n"
-            "smc%=:\n"
-            "mov x0,#0 \n"
-            "ADR x1,smc%=\n"
             "mov x2,#0 \n"
-            "add x0,x0,#1 \n"
-            "ldr x3,[x1]\n"
-            "code%=:\n"
-            "add x2,x2,#1\n"
-            "ADR x1,code%=\n"
-            "sub x1,x1,#12\n"
-             "str x3,[x1]\n"
-            "cmp x2,#10\n"
-            "bge out%=\n"
+            "mov x0,#0 \n"
+     "smc%=:"
+            "add x2,#1 \n"
+            "adr x3,smc%=\n"
+            "ldr x1,[x3]\n"
+     "code%=:\n"
+            "add x0,#1\n"
+            "adr x3,code%=\n"
+            "str x1,[x3]\n"
             "cmp x0,#10\n"
             "bge out%=\n"
+            "cmp x2,#10\n"
+            "bge out%=\n"
             "b code%=\n"
-            "out%=:\n"
-            "mov x0,x2\n"
-            "ldp    x29, x30, [sp, #0x20]  \n"
-            "add    sp, sp, #0x30   \n"
-            "mov %0,x0 \n"
-            :"=r"(a)
+     "out%=:\n"
+              "ldp    x29, x30, [sp, #0x20]  \n"
+               "add    sp, sp, #0x30   \n"
+             "mov %0,x0 \n"
+                :"=r"(a)
     );
 
 
