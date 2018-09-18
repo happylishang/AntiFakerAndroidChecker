7bd43e0fa1d34296b5b0af4a62b1d7caa4604ee3
happylishang <happylishang@163.com>
2018-09-18 13:07:55 +0800

    取消冗余
    是的

--- app/src/main/jni/emulator/com.snail.device_jni_EmulatorDetectUtil.c
+++ app/src/main/jni/emulator/com.snail.device_jni_EmulatorDetectUtil.c
@@ -35,22 +35,6 @@ int load(JNIEnv *env) {
 }
 
 
-
-
-void clearcache(char* begin, char *end)
-{
-	const int syscall = 0xf0002;
-	__asm __volatile (
-		"mov	 r0, %0\n"
-		"mov	 r1, %1\n"
-		"mov	 r7, %2\n"
-		"mov     r2, #0x0\n"
-		"svc     0x00000000\n"
-		:
-		:	"r" (begin), "r" (end), "r" (syscall)
-		:	"r0", "r1", "r7"
-		);
-}
 int (*asmcheck)(void);
 
 JNIEXPORT jboolean JNICALL Java_com_snail_device_jni_EmulatorDetectUtil_detect
