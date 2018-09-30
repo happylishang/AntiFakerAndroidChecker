4525937a61dc4253b28231fcea65fc38110a65b8
happysnail <happylishang@163.com>
2018-09-30 16:51:21 +0800

    sd

---
+++ antifake/src/main/obj/local/x86/objs/emulator_check/emulator/com.snail.antifake_jni_EmulatorDetectUtil.o.d
@@ -0,0 +1,158 @@
+/Users/personal/prj/GitHub/CacheEmulatorChecker/antifake/src/main/obj/local/x86/objs/emulator_check/emulator/com.snail.antifake_jni_EmulatorDetectUtil.o: \
+ /Users/personal/prj/GitHub/CacheEmulatorChecker/antifake/src/main/jni/emulator/com.snail.antifake_jni_EmulatorDetectUtil.c \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/jni.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/unistd.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdint.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_wchar_limits.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/select.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/time.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/time.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/signal.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/limits.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/limits.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/limits.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/internal_types.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/limits.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/syslimits.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page_32.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/string.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/malloc.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/signal.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/signal.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/sigcontext.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/siginfo.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/siginfo.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/ucontext.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/user.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/sysconf.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/capability.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/pathconf.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/mman.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/mman.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/mman.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/mman-common.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/android/log.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/fcntl.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/fcntl.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/fcntl.h \
+ /Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/fcntl.h
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/jni.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/unistd.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_wchar_limits.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/select.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/time.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/time.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/signal.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/limits.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/limits.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/limits.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/internal_types.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/limits.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/syslimits.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page_32.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/string.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/signal.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/signal.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/sigcontext.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/siginfo.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/siginfo.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/ucontext.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/user.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/sysconf.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/capability.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/pathconf.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/mman.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/mman.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/mman.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/mman-common.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/android/log.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/fcntl.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/fcntl.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/fcntl.h:
+
+/Users/personal/software/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/fcntl.h:
