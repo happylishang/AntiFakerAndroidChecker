6803dad10b3f11a8d70ee887f4284aa0d011c47c
李尚 <happylishang@163.com>
2022-04-25 10:35:54 +0800

    update

--- antifake/src/main/obj/local/armeabi-v7a/objs/emulator_check/emulator/com.snail.antifake_jni_EmulatorDetectUtil.o.d
+++
@@ -1,2 +0,0 @@
-/Users/personal/prj/demo/CacheEmulatorChecker/antifake/src/main/obj/local/armeabi-v7a/objs/emulator_check/emulator/com.snail.antifake_jni_EmulatorDetectUtil.o: \
-  /Users/personal/prj/demo/CacheEmulatorChecker/antifake/src/main/jni/emulator/com.snail.antifake_jni_EmulatorDetectUtil.c
--- antifake/src/main/obj/local/armeabi/objs/emulator_check/emulator/com.snail.antifake_jni_EmulatorDetectUtil.o.d
+++
@@ -1,2 +0,0 @@
-/Users/personal/prj/demo/CacheEmulatorChecker/antifake/src/main/obj/local/armeabi/objs/emulator_check/emulator/com.snail.antifake_jni_EmulatorDetectUtil.o: \
-  /Users/personal/prj/demo/CacheEmulatorChecker/antifake/src/main/jni/emulator/com.snail.antifake_jni_EmulatorDetectUtil.c
