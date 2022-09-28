23b60f3384c3ee68c6946fd5aa6d7e1e7f31993b
李尚 <happlishang@163.com>
2022-09-28 09:47:18 +0800

    Cohesion

--- gradlew
+++ gradlew
@@ -6,7 +6,7 @@
 ####
 ##############################################################################
 
-# Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+# Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script. 1
 DEFAULT_JVM_OPTS=""
 
 APP_NAME="Gradle"
--- gradlew.bat
+++ gradlew.bat
@@ -9,7 +9,7 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.1
 set DEFAULT_JVM_OPTS=
 
 set DIRNAME=%~dp0
